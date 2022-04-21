19bdcb935825f35ab87f1e12a9a9700554c4c054
FNH2022 <98783960+FNH2022@users.noreply.github.com>
2022-04-20 23:24:32 -0700

    Update s.py

--- s.py
+++ s.py
@@ -8,6 +8,5 @@ button = digitalio.DigitalInOut(board.GPIO_P36)
 button.direction = digitalio.Direction.INPUT
 # use an external pullup since we don't have internal PU's
 #button.pull = digitalio.Pull.UP
-while button==0 :
-  time.wait()
+while  button == 0 : 
   print("got it")
