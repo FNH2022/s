1d42a43e3373edda44aa5d46d1607bbebda932a3
FNH2022 <98783960+FNH2022@users.noreply.github.com>
2022-04-20 22:54:58 -0700

    Update speedometer.py

--- speedometer.py
+++ speedometer.py
@@ -8,4 +8,5 @@ button = digitalio.DigitalInOut(board.GPIO_P36)
 button.direction = digitalio.Direction.INPUT
 # use an external pullup since we don't have internal PU's
 #button.pull = digitalio.Pull.UP
- not button("we got Input")
+se = not button
+se("we got Input")
