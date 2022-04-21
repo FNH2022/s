38abb7f896afa627a4c4f790d3033b7806887648
FNH2022 <98783960+FNH2022@users.noreply.github.com>
2022-04-20 19:15:40 -0700

    Add files via upload

---
+++ speedometer.py
@@ -0,0 +1,13 @@
+import time
+import board
+import digitalio
+
+print("press the button!")
+
+button = digitalio.DigitalInOut(board.GPIO_P36)
+button.direction = digitalio.Direction.INPUT
+# use an external pullup since we don't have internal PU's
+#button.pull = digitalio.Pull.UP
+
+if not button :
+    print("we got Input")
